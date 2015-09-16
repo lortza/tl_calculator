e728e57e4ef11dcbc0859acf3df5e7979a35e143
anne <richardson.ae@gmail.com>
2015-09-16 14:15:42 -0500

    assignment complete: used while loop and if loop

--- calculator.rb
+++ calculator.rb
@@ -1,38 +1,48 @@
-#1. string interpolation vs concat
-#2. extract repetitive logic to methods
-#3. keep track of variable types (class)
-#3.1. using pry
-#4. variable scope determined by do...end; outer scope is available to inner scope, but not vice versa
+#Code up your own calculator from the lecture. Make sure you can run it from the command line. Save the calculator file in a directory, and initialize the directory as a git repository. Make sure this isn't nested in another existing git repository. Then, push this git repository to Github.
 
+#Hint: review this section on “do/while” loops on how to keep asking for input until some condition is met: http://www.gotealeaf.com/books/ruby/read/loops_iterators#dowhileloops
 
+# require "pry"
+# binding.pry
 
-require "pry"
+options = ["A", "B", "C", "D"]
 
-def say(msg)
-  puts "=> #{msg}"
+def actions
+  puts "Please choose one of these options and hit Enter: "
+  puts "  A) add" 
+  puts "  B) subtract" 
+  puts "  C) multiply" 
+  puts "  D) divide"
 end
 
-say "What's the first number?"
+puts "Enter your first number: "
 num1 = gets.chomp
 
-say "What's the second number?"
+puts "Enter your second number: "
 num2 = gets.chomp
 
-say "1) add 2) subtract 3) multiply 4) divide"
+actions
 operator = gets.chomp
+logic = options.include? operator.upcase
 
-if operator == "1"
+while logic == false
+  puts "I'm sorry, your selection was not valid."
+  actions
+  operator = gets.chomp
+  logic = options.include? operator.upcase
+end#while
+
+if operator.upcase == "A"
   result = num1.to_i + num2.to_i
-  binding.pry
-elsif operator == "2"
+  puts "#{num1} + #{num2} = #{result}"
+elsif operator.upcase == "B"
   result = num1.to_i - num2.to_i
-elsif operator == "3"
+  puts "#{num1} - #{num2} = #{result}"
+elsif operator.upcase == "C"
   result = num1.to_i * num2.to_i
-elsif operator == "4"
-  result = num1.to_f / num2.to_f
+  puts "#{num1} x #{num2} = #{result}"
 else
-  say "That was an invalid option."
-    #say "Please choose one of these 4 options:"
+  result = num1.to_f / num2.to_f
+  puts "#{num1} / #{num2} = #{result}"
 end
 
-puts "The result is #{result}."
